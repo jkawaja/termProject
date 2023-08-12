c1bb1b6027fa3cf6fb8fabd97579a12ab13ed877
Justin Kawaja <jkawaja909@gmail.com>
2023-08-12 14:15:39 -0230

    removed .html5 from fields in form.py

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileField
 from wtforms.fields import StringField, TextAreaField
-from wtforms.fields.html5 import DateField, EmailField, TelField
+from wtforms.fields import DateField, EmailField, TelField
 from wtforms.validators import DataRequired, Length
 
 class RecipeForm(FlaskForm):
